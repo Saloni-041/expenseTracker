63ba294d50333acef9b3f786de84eecf7bf0f8cd
Saloni-eGov <saloni.bajaj@egovernments.org>
2024-05-24 11:35:47 +0530

    made exception handler for class MethodArgumentTypeMismatchException and Exception class

--- src/main/java/com/example/expenseTrackerApi/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/expenseTrackerApi/exceptions/GlobalExceptionHandler.java
@@ -6,9 +6,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.Date;
 
+//It internally uses component annotation as soon as app runs spring will create object of this class
 @ControllerAdvice
 public class GlobalExceptionHandler
 {
@@ -20,5 +22,25 @@ public class GlobalExceptionHandler
         object.setMessage(ex.getMessage());
         object.setTimeStamp(new Date());
         return new ResponseEntity<ErrorObject>(object,HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorObject> handleException(MethodArgumentTypeMismatchException ex,WebRequest request)
+    {
+        ErrorObject errorObject=new ErrorObject();
+        errorObject.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        errorObject.setMessage(ex.getMessage());
+        errorObject.setTimeStamp(new Date());
+        return new ResponseEntity<ErrorObject>(errorObject,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorObject> handleGeneralException(Exception ex,WebRequest request)
+    {
+        ErrorObject errorObject=new ErrorObject();
+        errorObject.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        errorObject.setMessage(ex.getMessage());
+        errorObject.setTimeStamp(new Date());
+        return new ResponseEntity<ErrorObject>(errorObject,HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
